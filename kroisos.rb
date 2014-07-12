8f829fce49e70466456018bb629914b0c125a536
Davide Taviani <helios89@gmail.com>
2014-07-12 18:35:10 +0200

    Better session management

--- kroisos.rb
+++ kroisos.rb
@@ -8,6 +8,7 @@ require 'date'
 require_relative 'models'
 # If you want the logs displayed you have to do this before the call to setup
 DataMapper::Logger.new($stdout, :debug)
+
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/kroisos.db")
 DataMapper.finalize.auto_upgrade!
 
@@ -15,13 +16,15 @@ User.create(name: "Davide")
 User.create(name: "Kleopatra")
 
 enable :sessions
+set :session_secret, "supersecretphrase"
 
 get '/' do
+  @user = User.get(session[:user])
   haml :home, layout: :layout
 end
 
 post '/select_user' do
-  @user = params["user_select"]
-  session[:user] = @user
+  @user = User.first(name: params["user_select"])
+  session[:user] = @user ? @user.id : nil
   redirect to '/'
 end
